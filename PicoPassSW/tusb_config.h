7aea92fb9ba3070078081df4fddb59cf0a55cda9
Mustaeen Siddiqui <mustaeensiddiqui27@gmail.com>
2025-08-14 00:10:08 +0100

    Added tusb config for serial communication.

--- PicoPassSW/tusb_config.h
+++ PicoPassSW/tusb_config.h
@@ -1,17 +1,26 @@
 #pragma once
 
-#define CFG_TUSB_MCU              OPT_MCU_RP2040
-#define CFG_TUSB_RHPORT0_MODE     OPT_MODE_DEVICE
-#define CFG_TUSB_RHPORT0_SPEED    OPT_MODE_FULL_SPEED
+#define CFG_TUSB_MCU OPT_MCU_RP2040
+#define CFG_TUSB_RHPORT0_MODE OPT_MODE_DEVICE
+#define CFG_TUSB_RHPORT0_SPEED OPT_MODE_FULL_SPEED
 
 #ifndef CFG_TUSB_OS
 #define CFG_TUSB_OS OPT_OS_NONE
 #endif
 
 #define CFG_TUSB_MEM_SECTION
-#define CFG_TUSB_MEM_ALIGN        __attribute__ ((aligned(4)))
+#define CFG_TUSB_MEM_ALIGN __attribute__((aligned(4)))
 
-#define CFG_TUD_HID               1  // 👈 Enable HID support
+#define CFG_TUD_HID 1 // 👈 Enable HID support
 
 // HID buffer size
-#define CFG_TUD_HID_EP_BUFSIZE    64
+#define CFG_TUD_HID_EP_BUFSIZE 64
+
+#define CFG_TUD_CDC 1
+#define CFG_TUD_MSC 0
+#define CFG_TUD_VENDOR 0
+#define CFG_TUD_ENDPOINT0_SIZE 64
+
+// CDC buffers (reasonable sizes)
+#define CFG_TUD_CDC_RX_BUFSIZE 256
+#define CFG_TUD_CDC_TX_BUFSIZE 256
